96e0419de1676aeedb561e379fb9fb77612ef799
AlexeyWatt <allexdoma2@gmail.com>
2022-04-25 18:02:07 +0300

    add zadacha druzia

---
+++ zadacha_druzia/Program.cs
@@ -0,0 +1,20 @@
+﻿int v1 = 2, v2 = 3, vs = 5, s = 1000, count = 0;
+bool f = true;
+
+while (s >= 10)
+{
+    if (f == true)      ///f=true собака двигается от первого путника
+    {
+        int t = s / (v2 + vs);
+        s -= (v1 + v2) * t;
+        f = false;
+    }
+    else
+    {
+        int t = s / (v1 + vs);
+        s -= (v1 + v2) * t;
+        f = true;
+    }
+    count += 1;
+}
+Console.WriteLine(count);
