8fe7bccff9e88180bc15f81325df750bfcc58f96
AlexeyWatt <allexdoma2@gmail.com>
2022-04-25 17:28:30 +0300

    add code triangle

---
+++ triangle/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30,
+    count = 0;
+int x = xa;
+int y = ya;
+
+
+while (count < 1000)
+{
+    int sel = new Random().Next(0, 3);
+    if (sel == 0)
+    {
+        x = (xa + x) / 2;
+        y = (ya + y) / 2;
+        Console.SetCursorPosition(x, y);
+        Console.WriteLine("+");
+    }
+    if (sel == 1)
+    {
+        x = (xb + x) / 2;
+        y = (yb + y) / 2;
+        Console.SetCursorPosition(x, y);
+        Console.WriteLine("+");
+    }
+    if (sel == 2)
+    {
+        x = (xc + x) / 2;
+        y = (yc + y) / 2;
+        Console.SetCursorPosition(x, y);
+        Console.WriteLine("+");
+    }
+    count += 1;
+}
